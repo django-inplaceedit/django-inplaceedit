b75b2b30260a3633e4b367dcbe096f55ef80996b
pmartin <pmartin@43d3a112-9a52-0410-bc29-92ccca371478>
2011-02-16 17:18:44 +0000

    Fixes #151 More hardy

--- inplaceeditform/media/js/DateTimeShortcutsInitial.js
+++ inplaceeditform/media/js/DateTimeShortcutsInitial.js
@@ -1,10 +1,17 @@
 function DateTimeShortcutsInitial() {
+    var datetime_load = null;
+    var calendar_load = null;
+    var gettext_load = null;
     try{
-        DateTimeShortcuts.init();
-        var calendar_load = CalendarNamespace;
+        datetime_load = DateTimeShortcuts;
+        gettext_load = gettext;
+        calendar_load = CalendarNamespace;
     }
     catch(err) {
-        setTimeout(DateTimeShortcutsInitial, 500);
+        setTimeout(DateTimeShortcutsInitial, 3000);
+    }
+    if (datetime_load != null && calendar_load != null && gettext_load !=null){
+        DateTimeShortcuts.init();
     }
 }
 DateTimeShortcutsInitial();
