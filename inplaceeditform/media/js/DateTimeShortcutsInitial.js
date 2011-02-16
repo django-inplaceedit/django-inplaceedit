df85fee244a074d2d3b894f8e506fb2a4d3922f3
pmartin <pmartin@43d3a112-9a52-0410-bc29-92ccca371478>
2011-02-16 17:07:23 +0000

    Fixes #151 Now work the widget of date-datetime in IE

--- inplaceeditform/media/js/DateTimeShortcutsInitial.js
+++ inplaceeditform/media/js/DateTimeShortcutsInitial.js
@@ -1,1 +1,10 @@
-DateTimeShortcuts.init();
+function DateTimeShortcutsInitial() {
+    try{
+        DateTimeShortcuts.init();
+        var calendar_load = CalendarNamespace;
+    }
+    catch(err) {
+        setTimeout(DateTimeShortcutsInitial, 500);
+    }
+}
+DateTimeShortcutsInitial();
