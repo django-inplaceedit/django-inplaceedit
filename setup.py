504c979e8caa9beb3776e18023abc98ea8494e92
pmartin <pmartin@43d3a112-9a52-0410-bc29-92ccca371478>
2011-06-23 11:23:57 +0000

    See #1 Change of version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
 
 setup(
     name="django-inplaceedit",
-    version="0.66",
+    version="0.67",
     author="Yaco Sistemas S.L.",
     author_email="pmartin@yaco.es",
     description="Django application that allows you to inline edition of some data from the database",
