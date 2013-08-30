1944d034fee335f9e071d20240f8d9dd35149fa3
Pablo Martín <goinnn@gmail.com>
2013-08-30 12:08:25 +0200

    Adding copyright

--- testing/testing/urls.py
+++ testing/testing/urls.py
@@ -1,4 +1,17 @@
-# -*- coding: utf-8 -*-
+# Copyright (c) 2010-2013 by Yaco Sistemas <goinnn@gmail.com> or <pmartin@yaco.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this programe.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf import settings
 try:
@@ -6,7 +19,6 @@ try:
 except ImportError:  # Django 1.5
     from django.conf.urls import include, patterns, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
