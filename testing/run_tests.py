ca9e1b8159cf276575ec75cad051ceab679ea478
Pablo Martín <goinnn@gmail.com>
2013-08-30 12:16:54 +0200

    First tox test

---
+++ testing/run_tests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2010-2013 by Yaco Sistemas <goinnn@gmail.com> or <pmartin@yaco.es>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this programe.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'testing.settings'
+
+from django.core import management
+
+management.call_command('test', 'testing.unit_tests')
