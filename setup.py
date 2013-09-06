cd127072a1e27fbd50576104d4ba4590faefc89c
Pablo Martín <goinnn@gmail.com>
2013-09-06 12:50:21 +0200

    Update the setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
-# Copyright (c) 2010 by Yaco Sistemas <pmartin@yaco.es>
+# Copyright (c) 2010-2013 by Yaco Sistemas <goinnn@gmail.com> or <pmartin@yaco.es>
 #
-# This software is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This software is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# along with this programe.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 from setuptools import setup, find_packages
@@ -23,8 +23,8 @@ def read(*rnames):
 setup(
     name="django-inplaceedit",
     version="1.0.0",
-    author="Yaco Sistemas S.L.",
-    author_email="pmartin@yaco.es",
+    author="Pablo Martin",
+    author_email="goinnn@gmail.com",
     description="Django application that allows you to inline edition of some data from the database",
     long_description=(read('README.rst') + '\n\n' + read('CHANGES.rst')),
     classifiers=[
